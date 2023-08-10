0dc0a7b78aeb7c01ed6d4725b79fb44ed130110e
fishfan <fishf3@163.com>
2023-08-10 12:41:23 +0800

    完成后台基础搭建

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,54 @@
-import { useRoutes } from 'react-router-dom'
+import { useRoutes,useLocation,useNavigate } from 'react-router-dom'
 import routes from './router/index'
+import { useEffect } from 'react'
+import { message } from 'antd'
+
+
+function ToPage1() {
+  const navigate = useNavigate()
+  useEffect(() => {
+    navigate('/page1')
+  },[])
+  return <div></div>
+}
+function ToLogin() {
+   const navigate = useNavigate()
+  useEffect(() => {
+    navigate('/login')
+  },[])
+  return <div></div>
+}
+function BeforeRouterEnter() {
+  const outlet = useRoutes(routes)
+  /**
+   * 后台管理系统两种经典的跳转方式
+   * 1 如果访问的是登录页面，有token 跳转到首页
+   * 2 如果访问的不是登录页面，没有token ，跳转到登录页面
+   * 
+   * 其他都是正常方形
+   */
+  const token = localStorage.getItem('token')
+  const location =useLocation()
+  if (location.pathname === '/login' && token) {
+    // 这里不能直接用useNavigate来实现跳转，因为BeforeRouterEnter使用正常的JSX组件
+    return <ToPage1/>
+  }
+  if (location.pathname !== '/login' && !token) {
+     message.warning('你还没有登录')
+    // 这里不能直接用useNavigate来实现跳转，因为BeforeRouterEnter使用正常的JSX组件
+    return <ToLogin/>
+  }
+  return <div>
+      {outlet}
+  </div>
+}
+
+
 
 function App() {
-  const outlet = useRoutes(routes)
   return (
     <div>
-      {outlet}
+      <BeforeRouterEnter/>
     </div>
   )
 }
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 
-import { Breadcrumb, Layout, theme } from 'antd';
-import { Outlet } from 'react-router-dom'
+import { Breadcrumb, Button, Layout, theme } from 'antd';
+import { Outlet,useNavigate } from 'react-router-dom'
 import Menu from '@/components/MainMenu'
 const { Header, Content, Footer, Sider } = Layout;
 
 
-
-
 const App: React.FC = () => {
-    const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(false);
+  const navigate = useNavigate()
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+  const reset = () => {
+    localStorage.removeItem('token')
+    navigate('/login')
+  }
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
@@ -23,7 +26,8 @@ const App: React.FC = () => {
         <Header style={{ padding: 0, background: colorBgContainer,marginBottom:'10px' }}>
             <Breadcrumb style={{ margin: '16px 0' }}>
                 <Breadcrumb.Item>User</Breadcrumb.Item>
-                <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            <Button onClick={reset}>注销</Button>
             </Breadcrumb>
         </Header>
         <Content style={{ margin: '0 16px' }}>
